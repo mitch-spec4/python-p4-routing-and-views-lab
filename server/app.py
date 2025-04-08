3c4bcf922d5b589c426e5ae9fc9806fe10259fa1
mitch kamau <michchore@gmail.com>
2025-04-08 12:44:52 +0300

    main

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,31 +8,33 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<string:parameter>')
-def print_string(parameter):
-    print (parameter)
+@app.route("/print/<parameter>")
+def print_parameter(parameter):
+    print(parameter)
     return parameter
 
-@app.route('/count/<int:parameter>')
-def count(parameter):
-    count = f''
-
-    for n in range(parameter):
-        count +=f'{n}\n'
+@app.route("/count/<int:parameter>")
+def count_parameter(parameter):
+    count = f""
+    for i in range(parameter):
+        count += f"{i}\n"
     return count
 
-@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
-def math(num1, operation, num2):
-
-    if operation == '+':
-        return str(num1 + num2)
-    elif operation == '-':
-        return  str(num1 - num2)
-    elif operation == '*':
-        return str(num1 * num2)
-    elif operation == 'div':
-        return str(num1 / num2)
-    elif operation == '%':
-        return str(num1 % num2)
+@app.route("/math/<int:parameter1>/<string:operation>/<int:parameter2>")
+def math(operation, parameter1, parameter2):
+    if operation == "+":
+        answer = parameter1 + parameter2
+    elif operation == "-":
+        answer = parameter1 - parameter2
+    elif operation == "*":
+        answer = parameter1 * parameter2
+    elif operation == "div":
+        answer = parameter1 / parameter2
+    elif operation == "%":
+        answer = parameter1 % parameter2
     else:
-      return f"<h2>no result</h2>"
+        return None
+    return str(answer)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
